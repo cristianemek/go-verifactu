cf0da9e01061e370c8533a18d655c10f84eb35d3
Cristianemek <cristianrodriguezlugo@gmail.com>
2026-08-16 14:00:12 +0200

    add method Format for type date

--- record/fecha.go
+++ record/fecha.go
@@ -15,3 +15,8 @@ func (f Fecha) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 	t := time.Time(f)
 	return e.EncodeElement(t.Format(fechaFormat), start)
 }
+
+func (f Fecha) Format() string {
+	t := time.Time(f)
+	return t.Format(fechaFormat)
+}
