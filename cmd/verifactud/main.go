68b864a7fec4c5d9770fa60d47038a0bff14f570
Cristianemek <cristianrodriguezlugo@gmail.com>
2026-09-11 23:35:19 +0200

    feat: agregar manejo de señales y bucle de remisión en el servidor

--- cmd/verifactud/main.go
+++ cmd/verifactud/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/cristianemek/go-verifactu"
 	"github.com/cristianemek/go-verifactu/aeat"
@@ -37,14 +42,49 @@ func main() {
 	mux.HandleFunc("GET /v1/{nif}/estado", srv.auth(srv.estado))
 	mux.HandleFunc("GET /v1/{nif}/conexion", srv.auth(srv.conexion))
 
-	slog.Info("Listening", "address", cfg.Listen)
-
-	err = http.ListenAndServe(cfg.Listen, mux)
+	cada, err := time.ParseDuration(cfg.RemisionCada)
 
 	if err != nil {
-		slog.Error("Error starting server", "error", err)
+		slog.Error("Error parsing remision interval", "error", err)
 		os.Exit(1)
 	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+
+	defer stop()
+
+	hecho := make(chan struct{})
+
+	go func() {
+		srv.bucleRemision(ctx, cada)
+		close(hecho)
+	}()
+
+	httpSrv := &http.Server{Addr: cfg.Listen, Handler: mux}
+
+	slog.Info("Listening", "address", cfg.Listen)
+
+	go func() {
+		if err := httpSrv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("Error starting server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-ctx.Done()
+
+	slog.Info("Shutting down server")
+
+	ctxShutdown, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = httpSrv.Shutdown(ctxShutdown)
+
+	if err != nil {
+		slog.Error("Error shutting down server", "error", err)
+	}
+	<-hecho
+
 }
 
 func construirServidor(cfg *Config) (*servidor, error) {
